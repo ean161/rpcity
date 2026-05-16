19476b6cb0e102c8cace37298984efa3c9caf8d9
Nguyễn Hoài Ân <52894214+ean161@users.noreply.github.com>
2026-05-16 18:25:55 +0700

    feat: add license verify

--- src/main/java/vn/ean/Main.java
+++ src/main/java/vn/ean/Main.java
@@ -1,5 +1,6 @@
 package vn.ean;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,18 +9,34 @@ import vn.ean.chicken_farm.ChickenFarm;
 import vn.ean.delivery.Delivery;
 import vn.ean.economy.Economy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class Main extends JavaPlugin {
 
     private Economy economy;
     private Delivery delivery;
     private ChickenFarm chickenFarm;
+    private String ip = "", licenseCode = "";
 
     @Override
     public void onEnable() {
+        initClientLicense();
+
         economy = new Economy();
         delivery = new Delivery(this);
         chickenFarm = new ChickenFarm(this);
-        getLogger().info("RPCity plugin enabled");
+
+        if (!hasLicense()) {
+            getLogger().severe("Can't verify license " + licenseCode + ", please contact via nghoaian161@gmail.com");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        getLogger().info("RPCity plugin enabled for license " + licenseCode);
         getServer().getPluginManager().registerEvents(economy, this);
         getServer().getPluginManager().registerEvents(delivery, this);
         getServer().getPluginManager().registerEvents(chickenFarm, this);
@@ -28,6 +45,32 @@ public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("RPCity plugin disabled");
+    }
+
+    public void initClientLicense()  {
+        try {
+            URL url = new URL("https://api.ipify.org");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            this.ip = br.readLine();
+        } catch (Exception e) {
+            this.ip = "";
+        }
+        licenseCode = "RPCity/" + ip;
+    }
+
+    public boolean hasLicense()  {
+        String status;
+        try {
+            String encoded = URLEncoder.encode(licenseCode, StandardCharsets.UTF_8);
+            URL url = new URL("https://mcp-license.ean.vn/?code=" + encoded);
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            status = br.readLine();
+            return ("APPROVED").equals(status.trim());
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
